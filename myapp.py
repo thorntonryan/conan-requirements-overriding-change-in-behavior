3f2f5a7b398c19b77091d675f3600512243c433f
Ryan Thornton <ThorntonRyan@JohnDeere.com>
2019-04-24 08:18:07 -0500

    Remove boilerplate descriptions

--- myapp.py
+++ myapp.py
@@ -3,11 +3,6 @@ from conans import ConanFile
 class MyAppConan(ConanFile):
     name = "MyApp"
     version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of MyApp here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
 
     requires = ("SharedCOMLib/1.0.2@user/testing",)
